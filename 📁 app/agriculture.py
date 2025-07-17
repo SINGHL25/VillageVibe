15621991df6a40b5208a137ef8da6eaae31b712f
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-17 21:04:37 +1000

    Update agriculture.py

--- 📁 app/agriculture.py
+++ 📁 app/agriculture.py
@@ -1,1 +1,15 @@
+import streamlit as st
+
+def agri_tips():
+    st.subheader("🌾 Seasonal Agricultural Tips")
+
+    season = st.selectbox("Choose Season", ["Kharif", "Rabi", "Zaid"])
+    if season == "Kharif":
+        st.write("- Best crops: Rice, Maize, Cotton")
+        st.write("- Irrigate well after rains.")
+    elif season == "Rabi":
+        st.write("- Best crops: Wheat, Mustard, Gram")
+        st.write("- Use compost before sowing.")
+    else:
+        st.write("- Short crops like watermelon, cucumber")
 
