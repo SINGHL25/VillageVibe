b321593bb5c5fbf4486a17283237f33ccd0d8ad9
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-17 21:05:01 +1000

    Update media_gallery.py

--- 📁 app/media_gallery.py
+++ 📁 app/media_gallery.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import os
+
+def show_gallery():
+    st.subheader("📸 Village Gallery")
+    for file in os.listdir("assets/images"):
+        if file.endswith(".jpg") or file.endswith(".png"):
+            st.image(f"assets/images/{file}", width=400, caption=file)
 
