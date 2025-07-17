3cdceb23e66fff9e52ffa1240346366b3929488e
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-17 21:04:25 +1000

    Update village_profile.py

--- 📁 app/village_profile.py
+++ 📁 app/village_profile.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import json
+
+def village_info():
+    st.subheader("🏞️ Village Profiles")
+    with open("data/villages.json") as f:
+        villages = json.load(f)
+
+    village = st.selectbox("Select a Village", list(villages.keys()))
+    st.json(villages[village])
 
