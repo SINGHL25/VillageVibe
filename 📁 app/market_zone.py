c418c7acde7224c951c87ef2bf4788ef02bbc492
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-17 21:04:49 +1000

    Update market_zone.py

--- 📁 app/market_zone.py
+++ 📁 app/market_zone.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import streamlit as st
+
+def market_view():
+    st.subheader("🛍️ Regional Product Market")
+    df = pd.read_csv("data/products.csv")
+    st.dataframe(df)
+
+    product = st.selectbox("Product Name", df["Product Name"].unique())
+    st.write(df[df["Product Name"] == product])
 
